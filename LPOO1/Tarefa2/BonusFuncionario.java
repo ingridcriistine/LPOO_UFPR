f2c4d14bfca31b3312e22204835a4dd733133149
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2025-03-31 19:51:32 -0300

    Update BonusFuncionario.java

--- LPOO1/Tarefa2/BonusFuncionario.java
+++ LPOO1/Tarefa2/BonusFuncionario.java
@@ -13,6 +13,52 @@
 //      e. Auxiliar de Limpeza 
 //          i. 0,5 salário
 
+import java.util.Scanner;
+
 public class BonusFuncionario {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Float salario;
+        Float bonus = 0.0f;
+        Integer cargo, qtdDepartamento, qtdPessoas;
+
+        System.out.println("\nCargos: ");
+        System.out.println("1 - Diretor ");
+        System.out.println("2 - Gerente ");
+        System.out.println("3 - Analista ");
+        System.out.println("4 - Programador ");
+        System.out.println("5 - Auxiliar de Limpeza ");
+
+        System.out.print("\nDigite o seu cargo: ");
+        cargo = scan.nextInt();
+
+        System.out.print("Digite o seu salário atual: ");
+        salario = scan.nextFloat();
+
+        switch (cargo) {
+            case 1:
+                System.out.print("Digite a quantidade de departamentos gerenciados: ");
+                qtdDepartamento = scan.nextInt();
+                bonus = (4 * salario) + (3000 * qtdDepartamento);
+                break;
+            case 2:
+                System.out.print("Digite a quantidade de pessoas gerenciadas: ");
+                qtdPessoas = scan.nextInt();
+                bonus = (2 * salario) + (100 * qtdPessoas);
+                break;
+            case 3:
+                bonus = salario;
+                break;
+            case 4:
+                bonus =  0.8f * salario;
+                break;
+            case 5:
+                bonus = 0.5f * salario;
+                break;
+            default:
+                break;
+        }
+
+        System.out.print("\nValor do bônus anual: R$ " + String.format("%.2f", bonus));
+    }
 }
