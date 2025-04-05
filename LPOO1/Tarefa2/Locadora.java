530f89e462e0e5f1f78ddeb60aa4162359644fdc
ingridcriistine <ingridrochacristine@gmail.com>
2025-04-05 16:03:09 -0300

    updates

--- LPOO1/Tarefa2/Locadora.java
+++ LPOO1/Tarefa2/Locadora.java
@@ -5,22 +5,35 @@
 // c. Formate a saída para aparecer com duas casas decimais e (R$) na frente. 
 // d. Faça comentários na classe e gere o JavaDoc. 
 
+/**
+ * A classe Locadora calcula o faturamento anual e os ganhos com multas mensais
+ * de uma locadora de DVDs.
+ * 
+ * @author Ingrid Rocha
+*/
+
 import java.util.Scanner;
 
 public class Locadora {
+    /**
+     * Método principal que executa os cálculos de faturamento e multas.
+    */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Integer qtd;
         Float aluguel, faturamentoAnual, multasMensais, faturamentoMensal;
 
+        // Inputs necessários para fazer os cálculos
         System.out.print("\nInforme a quantidade de DVDs: ");
         qtd = scan.nextInt();
         System.out.print("Informe o valor do aluguel: ");
         aluguel = scan.nextFloat();
 
+         // Cálculo de faturamento.
         faturamentoMensal = (qtd / 3) * aluguel;
         faturamentoAnual = faturamentoMensal * 12;
 
+        // Cálculo de multa.
         multasMensais = (((qtd / 3) / 10) * aluguel) * 0.1f;
 
         System.out.println("\nFaturamento anual: R$ " + String.format("%.2f", faturamentoAnual));
