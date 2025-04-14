cdc4b8e4055beb1bba5e94826d5e1923c09654f6
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2025-04-14 19:28:41 -0300

    Create somaPares.java

---
+++ LPOO1/Tarefa 3/somaPares.java
@@ -0,0 +1,2 @@
+//Faça uma classe executável que dado um conjunto de 20 elementos numéricos, informe a soma de todos os números pares. 
+//Faça a validação de dados inválidos informando o usuário, e fazendo-o repetir a digitação.
