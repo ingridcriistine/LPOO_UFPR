6b8cb1682ec50eba76c3af246905bf563be33f72
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2025-04-14 19:31:50 -0300

    Create Empresa.java

---
+++ LPOO1/Tarefa 3/Empresa.java
@@ -0,0 +1,8 @@
+//Escreva um programa com os seguintes passos: 
+//1) Peça ao usuário que informe quantos funcionários existem na empresa. 
+//2) Instancie um array de strings e um array de doubles com o tamanho informado. 
+//3) Peça ao usuário que informe o nome e o salário de cada funcionário e armazene o nome no array de strings e o salário no array de doubles. 
+//4)Calcule a média salarial da empresa. 
+//5) Imprima a lista de nomes/salários que estão acima da média salarial. 
+//Obs.: Faça o tratamento das entradas de dados. Se o usuário informar um salário inválido, peça para ele informar novamente. 
+//Imprima os salários com duas casas decimais. Os nomes devem conter ao menos 3 caracteres.
