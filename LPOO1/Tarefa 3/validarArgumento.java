adbcfc122c372fb5e5f70022822141f08b5a7aec
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2025-04-14 19:27:14 -0300

    Create validarArgumento.java

---
+++ LPOO1/Tarefa 3/validarArgumento.java
@@ -0,0 +1,4 @@
+//Construa um programa que seja capaz de efetuar a média de todos os argumentos inteiros ou reais recebidos do teclado e 
+//imprimi-los. Argumentos inválidos devem ser desconsiderados (Imprimir uma mensagem para cada valor inválido.), sem provocar
+//a exibição de exceções (Exiba uma mensagem para o usuário pedindo a ele que informe novamente o valor). O usuário deve 
+//digitar S quando desejar encerrar a entrada de dados;
