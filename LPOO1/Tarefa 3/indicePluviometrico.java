b551fec82dcf01e5b9e4b2df8e575bfb6b7c38b8
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2025-04-14 19:30:07 -0300

    Create indicePluviometrico.java

---
+++ LPOO1/Tarefa 3/indicePluviometrico.java
@@ -0,0 +1,2 @@
+//Escreva um programa que leia do teclado 7 valores reais correspondentes ao índice pluviométrico diário de uma semana. 
+//Após a leitura, o programa deve determinar o índice pluviométrico médio, máximo e mínimo com o respectivo dia da ocorrência;
