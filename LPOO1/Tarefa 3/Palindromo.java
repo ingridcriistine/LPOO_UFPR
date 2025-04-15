c59adbb8741a91bfc338f6eb5f99be08cd0748dd
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2025-04-15 19:41:00 -0300

    Update Palindromo.java

--- LPOO1/Tarefa 3/Palindromo.java
+++ LPOO1/Tarefa 3/Palindromo.java
@@ -4,3 +4,35 @@
 // "Socorram-me, subi no ônibus em Marrocos” 
 // "A Rita, sobre vovô, verbos atira." 
 // "Olé! Maracujá, caju, caramelo." 
+
+import java.util.Scanner;
+
+public class Palindromo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String frase;
+
+        System.out.println("Insira uma frase: ");
+        frase = scan.nextLine();
+
+        String fraseInvertida = "";
+
+		for (int i = frase.length() - 1; i >= 0; i--) {
+			fraseInvertida = fraseInvertida + frase.charAt(i);
+		}
+
+        System.out.println("");
+
+        String a = frase.replaceAll(" ", "");
+        String b = fraseInvertida.replaceAll(" ", "");
+
+        if(a.equalsIgnoreCase(b)) {
+            System.out.println("É um palíndromo!");
+        } else {
+            System.out.println("Não é um palíndromo!");
+        }
+
+        System.out.println("Frase: " + frase.toLowerCase());
+        System.out.println("Frase invertida: " + fraseInvertida.toLowerCase());
+    }
+}
