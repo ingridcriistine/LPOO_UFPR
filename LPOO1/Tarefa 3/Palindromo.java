73c41d3908b16ef36cd20d1d98e10d56523c3ec2
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2025-04-14 21:43:42 -0300

    Create Palindromo.java

---
+++ LPOO1/Tarefa 3/Palindromo.java
@@ -0,0 +1,6 @@
+// Faça um programa que leia uma frase e informe se ela é um palíndromo ou não. Uma frase é um palíndromo quando pode ser lida tanto da forma usual, 
+// quanto de trás para frente. O programa deve ignorar maiúsculas e minúsculas na avaliação. Insira as frases sem acentuação; 
+// Dica: será necessário verificar os métodos disponíveis na classe String; 
+// "Socorram-me, subi no ônibus em Marrocos” 
+// "A Rita, sobre vovô, verbos atira." 
+// "Olé! Maracujá, caju, caramelo." 
